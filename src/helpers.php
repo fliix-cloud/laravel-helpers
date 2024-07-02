a4531e4c9c7cee86638d7abe870c5ec4941d5536
root <root@cloud05.fliix.cloud>
2024-07-02 08:28:48 +0200

    Initial commit

---
+++ src/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!function_exists('example_helper')) {
+    function example_helper($value)
+    {
+        return "This is an example helper function and your value is: " . $value;
+    }
+}
+
+// Weitere Helper-Funktionen hier hinzufügen
